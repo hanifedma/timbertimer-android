43dbf914f62a37d00dc4ad35ce2bcfbf0cf56f25
pidin <pidinplusplusultra@gmail.com>
2026-08-13 16:24:16 +0900

    Call it 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.example.timbertimer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
